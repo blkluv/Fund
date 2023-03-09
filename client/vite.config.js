3b8c215238d9161ca1c26bda871e747e00d47b0e
Suyash Srivastava <suyash.srt@outlook.com>
2023-03-10 02:42:37 +0530

    10/03

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,49 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  chunkSizeWarningLimit: 1000 *1024,
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id.toString().split("node_modules/")[1].split("/")[0].toString();
+          }
+        },
+      },
+    },
+  },
   define: {
     global: "globalThis",
     "process.env": {},
   },
+  optimizeDeps: {
+    include: ["react", "react-dom"],
+  },
+  server: {
+    hmr: {
+      overlay: false,
+    },
+  },
+  terserOptions: {
+    compress: {
+      drop_console: true,
+      ecma: 2015,
+      keep_infinity: true,
+      passes: 3,
+      unsafe: true,
+      unsafe_arrows: true,
+      unsafe_comps: true,
+      unsafe_Function: true,
+      unsafe_math: true,
+      unsafe_methods: true,
+      unsafe_proto: true,
+      unsafe_regexp: true,
+      unsafe_symbols: true,
+      unsafe_undefined: true,
+    },
+  },
+  treeshake: true,
 });
